a6329bb8c684b505dbcb68dca42a060ad7c60445
Roman <rpkulich@gmail.com>
2017-12-05 16:50:59 +0100

    Update passgen.py

--- passgen.py
+++ passgen.py
@@ -3,7 +3,7 @@ from random import*
 predvolba = string.hexdigits
 heslo =  "".join(choice(predvolba) for delka in range(randint(6, 19)))
 text = "Vygenerované heslo je: "
-print("Tento python script generuje náhodná hesla s politikou velkých a malých písmen, čísel a velikostí 6 - 19 znaků.")
+print("Tento python script generuje náhodná hesla s politikou velkých a malých písmen, čísel a rozsahem 6 - 19 znaků.")
 print("=============================")
 print(text + heslo)
 print("=============================")
